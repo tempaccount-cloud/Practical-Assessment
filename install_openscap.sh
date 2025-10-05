387c57fc1ccf10f41e54a83986eaeeb88f6fd913
tempaccount-cloud <233868626+tempaccount-cloud@users.noreply.github.com>
2025-10-05 23:07:49 +0200

    Add files via upload

---
+++ install_openscap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eux
+
+ sudo mkdir -p /usr/share/xml/scap/ssg/content
+ cd /usr/share/xml/scap/ssg/content
+ 
+sudo git clone -b master https://github.com/ComplianceAsCode/content.git
+
+cd content
+
+sudo ./build_product ubuntu2204
+
+cd build
+XML_FILE="ssg-ubuntu2204-ds.xml"
+
+
+#ls -lah 
+oscap info ${XML_FILE}
+oscap info --profile xccdf_org.ssgproject.content_profile_cis_level1_server ${XML_FILE}
